3d1d2e8cadeea69a7a6793b5171c616dad42219f
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-11 19:01:37 +0200

    feature: add port

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const DEFAULT_PORT = 3000;
 
 // Configure app with body parser to handle JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Set port
+
+const port = process.env.PORT || DEFAULT_PORT;
+
